ae077d5d3a53fd120c9739bfa442b659e72d0f67
Evgeny Pokhilko <evpomail@gmail.com>
2014-05-05 20:10:13 +1000

    Copy files from gtest build

---
+++ Tests/deps/gtest/update.cmd
@@ -0,0 +1,5 @@
+copy /Y "C:\prj\deps\gtest-1.6.0\cmake\msvc\MinSizeRel\gtest.lib" .\MinSizeRel\
+copy /Y "C:\prj\deps\gtest-1.6.0\cmake\msvc\MinSizeRel\gtest.pdb" .\MinSizeRel\
+
+copy /Y "C:\prj\deps\gtest-1.6.0\cmake\msvc\Debug\gtest.lib" .\Debug\
+copy /Y "C:\prj\deps\gtest-1.6.0\cmake\msvc\Debug\gtest.pdb" .\Debug\
